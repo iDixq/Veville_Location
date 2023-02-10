a4013ab92cb1b967d93b772fa2074826b517bb30
Ines Bechane <iness.bechane@gmail.com>
2023-02-10 16:45:07 +0100

    create,read

--- src/Controller/AgenceController.php
+++ src/Controller/AgenceController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Agence;
+use App\Form\AgenceType;
+use App\Repository\AgenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class AgenceController extends AbstractController
 {
-    #[Route('/agence', name: 'app_agence')]
-    public function index(): Response
+    /**
+     * @Route("/agence", name="app_agence")
+     */
+    public function index(AgenceRepository $repoAgence, Request $request, EntityManagerInterface $manager): Response
     {
+        $agence = new Agence;
+        $form = $this->createForm(AgenceType::class, $agence);
+        $form->handleRequest($request);
+        $agences = $repoAgence->findAll();
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($agence);
+            $manager->flush();
+
+            $this->addFlash('success', "l'agence' N" . $agence->getId() ."a bien été ajoutée");
+
+            return $this->redirectToRoute("app_agence");
+        }
+
+      
         return $this->render('agence/index.html.twig', [
-            'controller_name' => 'AgenceController',
+          "agences" => $agences,
+          "formAgence" => $form->createView()
         ]);
     }
+
 }
--- src/Form/AgenceType.php
+++ src/Form/AgenceType.php
@@ -4,20 +4,69 @@ namespace App\Form;
 
 use App\Entity\Agence;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class AgenceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('adresse')
-            ->add('ville')
-            ->add('cp')
-            ->add('description')
-            ->add('photo');
+            ->add('titre', TextType::class, [
+                "label" => "Titre",
+                "required" => true,
+                "attr" => [
+                    "placeholder" => "titre",
+                    "class" => "form-control"
+                ]
+            ]) 
+            ->add('adresse', TextType::class,[
+                "label" => "Adresse",
+                "required" => true,
+                "attr" => [
+                    "placeholder" => "adresse",
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('ville', TextType::class,[
+                "label" => "Ville",
+                "required" => true,
+                "attr" => [
+                    "placeholder" => "ville",
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('cp', NumberType::class,[
+                "label" => "Code-postal",
+                "required" => true,
+                "attr" => [
+                    "placeholder" => "cp",
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('description', TextareaType::class,[
+                "label" => "Description",
+                "required" => true,
+                "attr" => [
+                    "placeholder" => "description",
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('photo', TextType::class,[
+                "label" => "Photo",
+                "required" => true,
+                "attr" => [
+                    "placeholder" => "photo",
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('Ajouter',SubmitType::class);
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
