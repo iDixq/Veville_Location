7a85af8509c1ce84b3124679305a985dbea40649
Ines Bechane <iness.bechane@gmail.com>
2023-02-13 16:58:03 +0100

    creat contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+     /**
+     * @Route("/contact", name="app_contact")
+     */
+    public function index(ContactRepository $contact): Response
     {
+        $contact = new Contact;
+        $form = $this->createForm(ContactType::class, $contact);
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            "contact" => $contact,
+            'formContact' => $form->createView()
         ]);
     }
 }
+
