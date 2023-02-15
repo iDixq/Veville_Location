2e88759a6e9cfa9efab35e760a69141e3119af6f
Lucas Hebrard <lucas.hebrard91@gmail.com>
2023-02-15 14:10:23 +0100

    modif accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -9,51 +9,69 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(): Response
+    public function accueilAfficher(): Response
     {
+
+        $objetAccueil = new Commande;
+        $commandes = $repoCommande->findAll();
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $objetAccueil->setDateEnregistrement(new \DateTime());
+            $manager->persist($objetAccueil);
+            $manager->flush();
+            
+
+
+        }
+        
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
             "formAccueil" => $form->createview(),
             "dataAccueil" => $dataAccueil
 
         ]);
+
+        return $this->redirectToRoute("app_accueil");
+        
     }
 
-    #[Route('/membre', name: 'membre_afficher')]
-    public function afficher_ajouter_membre(MembreRepository $repoMembre, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $passwordHasher): Response
-    {
-        $objet_membre = new Membre;
-        $membres = $repoMembre->findAll();
+    // #[Route('/membre', name: 'membre_afficher')]
+    // public function afficher_ajouter_membre(MembreRepository $repoMembre, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $passwordHasher): Response
+    // {
+    //     $objet_membre = new Membre;
+    //     $membres = $repoMembre->findAll();
         
 
-        $form = $this->createForm(MembreType::class, $objet_membre);
-        $form->handleRequest($request);
+    //     $form = $this->createForm(MembreType::class, $objet_membre);
+    //     $form->handleRequest($request);
         
 
-        if ($form->isSubmitted() && $form->isValid()) {
+    //     if ($form->isSubmitted() && $form->isValid()) {
             
-            // $hashedPassword = $passwordHasher->hashPassword(
-            //     $objet_membre,
-            //     $request->get("membre")["mdp"]
-            // );
+    //         // $hashedPassword = $passwordHasher->hashPassword(
+    //         //     $objet_membre,
+    //         //     $request->get("membre")["mdp"]
+    //         // );
         
-            // $objet_membre->setPassword($hashedPassword);
-            // $repoMembre->save($objet_membre, true);
-            $objet_membre->setDateEnregistrement(new \DateTime());
-            $manager->persist($objet_membre);
-            $manager->flush();
+    //         // $objet_membre->setPassword($hashedPassword);
+    //         // $repoMembre->save($objet_membre, true);
+    //         $objet_membre->setDateEnregistrement(new \DateTime());
+    //         $manager->persist($objet_membre);
+    //         $manager->flush();
 
-            $this->addFlash("Success", "Le membre N° " . $objet_membre->getId() . "a bien été ajouté");
-        }
+    //         $this->addFlash("Success", "Le membre N° " . $objet_membre->getId() . "a bien été ajouté");
+    //     }
 
-        return $this->render('membre/membre_afficher.html.twig', [
-            'controller_name' => 'MembreController',
-            "formMembre" => $form->createView(),
-            "membres" => $membres,
-        ]);
-        return $this->redirectToRoute("membre_afficher");
+    //     return $this->render('membre/membre_afficher.html.twig', [
+    //         'controller_name' => 'MembreController',
+    //         "formMembre" => $form->createView(),
+    //         "membres" => $membres,
+    //     ]);
+    //     return $this->redirectToRoute("membre_afficher");
 
-    }
+    // }
 
    
 
